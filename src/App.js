9dc9e7f8792fb37f1f410de743d8ac3eeacf2fe6
Kumar <kumar.jack@gmail.com>
2022-02-13 11:36:03 -0500

    Changes for the Hello World

--- src/App.js
+++ src/App.js
@@ -23,9 +23,8 @@ function App() {
       <Header/>
       <Content>
         <Container text textAlign='center'>
-          <Image centered size='tiny' src={iconDevx} style={{marginBottom:"2em"}}/>
-          <p>Create React Ethereum DApp on Polygon</p>
-          <p>Build your first app with Starter Kits</p>
+          
+          <p>hellow world</p>
         </Container>
       </Content>
       <Footer/>
